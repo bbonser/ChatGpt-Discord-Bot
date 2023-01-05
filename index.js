711d2210c9284696be6283c5371c5e0d6dd6f080
Brett Bonser <bbonser89@gmail.com>
2023-01-04 19:53:03 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+// Create a Discord Bot using OpenAI API that interacts on the discord server
+require('dotenv').config();
+
+// Prepare to connect to the Discord API
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+]})
+
+// Prepare connection to OpenAI API
+const { Configuration , OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    organization: process.env.OPENAI_ORG,
+    apiKey: process.env.OPENAI_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+// Check for when a message on discord is sent
+client.on('messageCreate' , async function(message){
+    try {
+        // Dont respond to yourself or other bots
+        if(message.author.bot) return;
+
+        const gptResponse = await openai.createCompletion({
+            model: "davinci",
+            prompt: `ChatGPT is a freindly chatbot.\n\
+ChatGPT: Hello, how are you?\n\
+${message.author.username}: ${message.content}\n\`,
+ChatGPT: `,    
+            temperature: 0.9,
+            max_tokens: 100,
+            stop: ["ChatGPT:", "Brett Bonser:"],
+        })
+
+        message.reply(`${gptResponse.data.choices[0].text}`);
+        return;
+    } catch(err){
+        console.log(err)
+    }
+});
+
+//Log the bot into Discord 
+client.login(process.env.DISCORD_TOKEN);
+console.log("ChatGPT Bot is Online in Discord")
